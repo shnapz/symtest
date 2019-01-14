a649344bd5156f25fcc684054d7522f0a024bd1d
Petro Knysh <peteknysh@gmail.com>
2019-01-14 23:16:19 +0200

    added moq

--- symtest.Tests/symtest/Providers/HttpTransportProviderTests.cs
+++ symtest.Tests/symtest/Providers/HttpTransportProviderTests.cs
@@ -1,10 +1,24 @@
 namespace symtest.Tests.symtest.Providers
 {
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
     using NUnit.Framework;
 
     [TestFixture]
     public class HttpTransportProviderTests
     {
-        
+        [Test]
+        public Task TestRequests()
+        {
+            return null;
+        }
+
+        public static IEnumerable<TestCaseData> GetDataForSuccessfulTests
+        {
+            get
+            {
+                yield return new TestCaseData();
+            }
+        }
     }
 }
