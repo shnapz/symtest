96db53d7bed79210197d37516c412bb5f79abfbd
Petro Knysh <peteknysh@gmail.com>
2019-01-15 00:00:15 +0200

    first dummy test && config

--- symtest.Tests/symtest/Providers/HttpTransportProviderTests.cs
+++ symtest.Tests/symtest/Providers/HttpTransportProviderTests.cs
@@ -1,15 +1,45 @@
 namespace symtest.Tests.symtest.Providers
 {
     using System.Collections.Generic;
+    using System.Net.Http;
     using System.Threading.Tasks;
+    using global::symtest.Providers;
+    using Microsoft.Extensions.Logging;
+    using Moq;
     using NUnit.Framework;
 
     [TestFixture]
     public class HttpTransportProviderTests
     {
+        private IHttpClientFactory _httpClientFactoryMock;
+        private ILogger<HttpTransportProvider> _httpTransportProviderLoggerMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _httpClientFactoryMock = new Mock<IHttpClientFactory>().Object;
+            _httpTransportProviderLoggerMock = new Mock<ILogger<HttpTransportProvider>>().Object;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _httpClientFactoryMock = null;
+            _httpTransportProviderLoggerMock = null;
+        }
+
         [Test]
+        public void Stub()
+        {
+            Assert.IsTrue(true);
+        }
+        
+        [TestCaseSource(nameof(GetDataForSuccessfulTests))]
         public Task TestRequests()
         {
+            
+            
+            
             return null;
         }
 
